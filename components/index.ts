b17bc6ff18e6e97181923045fe441178f2ca383e
Neljon Lusterio <neljon@elephantventures.com>
2021-03-04 21:08:15 +0800

    Added Dropdown component

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './Button';
 export { default as Checkbox } from './Checkbox';
+export { default as Dropdown } from './Dropdown';
 export { default as Input } from './Input';
 export { default as Screen } from './Screen';
