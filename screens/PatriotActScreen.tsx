c06c8a63cf9a5f0bf5f93d3ccf03d363d9af9408
Neljon Lusterio <neljon@elephantventures.com>
2021-03-04 16:14:54 +0800

    Acknowledge Patriot Act

--- screens/PatriotActScreen.tsx
+++ screens/PatriotActScreen.tsx
@@ -1,16 +1,62 @@
 import { StackNavigationProp } from '@react-navigation/stack';
+import { Formik } from 'formik';
 import * as React from 'react';
 import { View } from 'react-native';
 import Button from '../components/Button';
 import Screen from '../components/Screen';
 import { Body, BodySmall, Heading3 } from '../components/Typography';
+import { useComplianceWorkflow } from '../contexts/ComplianceWorkflow';
 import { RootStackParamList } from '../types';
+import RizeClient from '../utils/rizeClient';
+import * as Network from 'expo-network';
 
 interface PatriotActScreenProps {
     navigation: StackNavigationProp<RootStackParamList, 'PatriotAct'>;
 }
 
 export default function PatriotActScreen({ navigation }: PatriotActScreenProps): JSX.Element {
+    const { complianceWorkflow, setComplianceWorkflow } = useComplianceWorkflow();
+    const rize = RizeClient.getInstance();
+
+    const onSubmit = async (): Promise<void> => {
+        if (complianceWorkflow) {
+            const externalStorageName = 'usa_ptrt_0';
+
+            // Check if the Patriot Act document is still in pending
+            let patriotActDocument = complianceWorkflow
+                .current_step_documents_pending
+                .find(x => x.external_storage_name === externalStorageName);
+
+            if (patriotActDocument) {
+                const ipAddress = await Network.getIpAddressAsync();
+                const updatedComplianceWorkflow = await rize.complianceWorkflow.acknowledgeComplianceDocuments(
+                    complianceWorkflow.uid,
+                    complianceWorkflow.customer.uid,
+                    {
+                        accept: 'yes',
+                        documentUid: patriotActDocument.uid,
+                        ipAddress: ipAddress,
+                        userName: complianceWorkflow.customer.email
+                    }
+                );
+
+                await setComplianceWorkflow(updatedComplianceWorkflow);
+
+                navigation.navigate('PII');
+            } else {
+                // Check if already accepted
+                patriotActDocument = complianceWorkflow
+                    .accepted_documents
+                    .find(x => x.external_storage_name === externalStorageName);
+
+                // If already accepted, redirect to next step
+                if (patriotActDocument) {
+                    navigation.navigate('PII');
+                }
+            }
+        }
+    };
+    
     return (
         <Screen useScrollView>
             <Heading3
@@ -62,9 +108,18 @@ export default function PatriotActScreen({ navigation }: PatriotActScreenProps):
 
             <BodySmall>&nbsp;</BodySmall>
 
-            <Button
-                title='I Agree'
-            />
+            <Formik
+                initialValues={{}}
+                onSubmit={onSubmit}
+            >
+                {({ isSubmitting, handleSubmit }) => (
+                    <Button
+                        title='I Agree'
+                        disabled={isSubmitting}
+                        onPress={(): void => handleSubmit()}
+                    />
+                )}
+            </Formik>
         </Screen>
     );
 }
