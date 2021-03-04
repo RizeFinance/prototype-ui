d6b407e7d8d0ce88eee948f6d6d6d6e382fd630b
Stephel Maca <stephelgeorgemaca@gmail.com>
2021-03-04 20:24:09 +0800

    Added the disclosure screen

--- screens/DisclosuresScreen.tsx
+++ screens/DisclosuresScreen.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
-import { Text } from 'react-native';
 import Screen from '../components/Screen';
+import { Heading3, BodySmall } from '../components/Typography';
 
 export default function DisclosuresScreen(): JSX.Element {
     return (
         <Screen>
-            <Text>Disclosures Screen</Text>
+            <Heading3 textAlign='center'>Rize Disclosures</Heading3>
+
+            {/* checkbox I agree to Terms of Use */}
+            {/* checkbox I agree to Privacy Policy*/}
+            {/* checkbox I agree to E-sign Disclosures and Agreement */}
+
+            <BodySmall>By clicking &quotI Agree&quot I have read and agreed to the Terms of Use, Privacy Policy and E-sign Disclosures and Agreement.</BodySmall>
         </Screen>
     );
 }
