6b25b259bee329a467152847d7562c5a79f7b22c
Neljon Lusterio <neljon@elephantventures.com>
2021-03-09 18:42:57 +0800

    Updated ProcessingApplication style

--- screens/ProcessingApplicationScreen.tsx
+++ screens/ProcessingApplicationScreen.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
-import { Text } from 'react-native';
+import { ActivityIndicator, StyleSheet } from 'react-native';
 import { Screen } from '../components';
+import { Heading3, Heading4 } from '../components/Typography';
 import { useCustomer } from '../contexts/Customer';
 import RizeClient from '../utils/rizeClient';
 
@@ -9,6 +10,15 @@ export default function ProcessingApplicationScreen(): JSX.Element {
     const rize = RizeClient.getInstance();
 
     let timeout = null;
+
+    const styles = StyleSheet.create({
+        container: {
+            marginTop: 100,
+        },
+        heading: {
+            marginVertical: 24,
+        }
+    });
 
     const refreshCustomerPeriodically = async (): Promise<void> => {
         await refreshCustomer();
@@ -28,8 +38,14 @@ export default function ProcessingApplicationScreen(): JSX.Element {
     }, []);
 
     return (
-        <Screen>
-            <Text>Processing Application Screen</Text>
+        <Screen style={styles.container}>
+            <ActivityIndicator size='large' />
+            <Heading3 textAlign='center' style={styles.heading}>
+                We&apos;re processing your application.
+            </Heading3>
+            <Heading4 textAlign='center'>
+                This should only take a few moments.
+            </Heading4>
         </Screen>
     );
 }
