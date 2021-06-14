ba6e908040c673d1a19094d4bf01eb6d6717f793
Kyle Salmon <kyleraysalmon@gmail.com>
2021-06-14 18:08:53 -0400

    Update Link Styles

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -14,12 +14,22 @@ export default function PlaidLink({
     onExit,
     onSuccess,
 }: PlaidLinkProps): JSX.Element {
+    const overrideStyles = {
+        backgroundColor: "#586CB7",
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
+        padding: 15,
+        marginTop: 25,
+    }
+
     return (
         <WebPlaidLink
             token={linkToken}
             onSuccess={onSuccess}
             onExit={onEvent}
             onEvent={onExit}
+            style={overrideStyles}
         >
           Connect External Account
         </WebPlaidLink>
